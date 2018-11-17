566b6cffd62f7cb0b58a5dd972a2cf4ffc7b05a8
Nolan Byrnes <nolanehs@gmail.com>
2018-11-17 08:09:22 -0500

    Wrote tests for POST request Contacts

--- spec/requests/contacts_requests_spec.rb
+++ spec/requests/contacts_requests_spec.rb
@@ -5,19 +5,15 @@ def json
 end
 
 RSpec.describe 'contacts API', type: :request do
-  # initialize test data
 
   let!(:contacts) { create_list(:contact, 10) }
   let(:contact_id) { contacts.first.id }
   let(:created_at) {Time.now}
 
-  # Test suite for GET /contacts
   describe 'GET /contacts' do
-    # make HTTP get request before each example
     before { get '/contacts' }
 
     it 'returns contacts' do
-      # Note `json` is a custom helper to parse JSON responses
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
@@ -26,4 +22,25 @@ RSpec.describe 'contacts API', type: :request do
          expect(response).to have_http_status(200)
        end
      end
+end
+
+
+
+RSpec.describe 'creating contacts', type: :request do
+  let(:valid_attributes) { {first_name: 'Woody', last_name: 'Cowboy', phone_number: '1234567891', created_at: Time.now} }
+
+  context 'when request attrs are valid' do
+    before {post '/contacts', params: valid_attributes}
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+  end
+
+context 'when an invalid request' do
+  before {contact '/contacts', params: {} }
+  it 'returns status code of 422' do
+    expect(response).to have_http_status(422)
+  end
+end
+
 end
